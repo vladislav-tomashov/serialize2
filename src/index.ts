2e4c1f8658dad23f1c720a05cf3b1f5b18e017cc
Vlad Tomashov <vladislav.tomashov@gmail.com>
2020-04-21 10:03:18 +0300

    enables all tests

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ import test3 from "./examples/testClass3";
 import test4 from "./examples/testClass4";
 
 (async function () {
-  // await test1();
-  // await test2();
-  // await test3();
+  await test1();
+  await test2();
+  await test3();
   await test4();
 })();
